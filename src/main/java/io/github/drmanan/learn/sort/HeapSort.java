15871a53759dd7f594d1cad47e6000bffe74e1ee
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2023-08-10 07:11:44 +0530

    HeapSort with premitive integer

--- src/main/java/io/github/drmanan/learn/sort/HeapSort.java
+++ src/main/java/io/github/drmanan/learn/sort/HeapSort.java
@@ -8,46 +8,28 @@
 
 package io.github.drmanan.learn.sort;
 
-import static io.github.drmanan.learn.util.Array.print_array;
+import io.github.drmanan.learn.util.Array;
 
 public class HeapSort {
 
-    // To heapify a subtree rooted with node i which is
-    // an index in arr[]. n is size of heap
-    void heapify(int arr[], int n, int i)
-    {
-        int largest = i; // Initialize largest as root
-        int l = 2 * i + 1; // left = 2*i + 1
-        int r = 2 * i + 2; // right = 2*i + 2
-
-        // If left child is larger than root
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
-
-        // If right child is larger than largest so far
-        if (r < n && arr[r] > arr[largest])
-            largest = r;
-
-        // If largest is not root
-        if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-
-            // Recursively heapify the affected sub-tree
-            heapify(arr, n, largest);
-        }
+    // Driver program
+    public static void main(String[] args) {
+        int[] arr = Array.generate_random_array(5, 20);
+        Array.print_array('b', arr);
+        HeapSort heapSort = new HeapSort();
+        heapSort.sort(arr);
+        Array.print_array('a', arr);
     }
 
-    public void sort(int arr[]) {
+    public void sort(int[] arr) {
         int n = arr.length;
 
         // Build heap (rearrange array)
-        for (int i = n / 2 - 1; i >= 0; i--)
+        for (int i = (n / 2) - 1; i >= 0; i--)
             heapify(arr, n, i);
 
         // One by one extract an element from heap
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = (n - 1); i >= 0; i--) {
             // Move current root to end
             int temp = arr[0];
             arr[0] = arr[i];
@@ -55,6 +37,30 @@ public class HeapSort {
 
             // call max heapify on the reduced heap
             heapify(arr, i, 0);
+        }
+    }
+
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    void heapify(int[] arr, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest]) largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest]) largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected subtree
+            heapify(arr, n, largest);
         }
     }
 }
