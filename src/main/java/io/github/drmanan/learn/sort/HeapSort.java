81b5078d59e9bb053648f0894f1bae447c8c977d
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2023-08-10 07:05:52 +0530

    HeapSort TEMP

--- src/main/java/io/github/drmanan/learn/sort/HeapSort.java
+++ src/main/java/io/github/drmanan/learn/sort/HeapSort.java
@@ -12,42 +12,49 @@ import static io.github.drmanan.learn.util.Array.print_array;
 
 public class HeapSort {
 
-    static int total;
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    void heapify(int arr[], int n, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
 
-    static void swap(Comparable[] arr, int a, int b) {
-        Comparable temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
-    }
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
 
-    static void heapify(Comparable[] arr, int i) {
-        int lft = i * 2;
-        int rgt = i * 2 + 1;
-        int grt = i;
-        if (lft <= total && arr[lft].compareTo(arr[grt]) >= 0) grt = lft;
-        if (rgt <= total && arr[rgt].compareTo(arr[grt]) >= 0) grt = rgt;
-        if (grt != i) {
-            swap(arr, i, grt);
-            heapify(arr, grt);
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
         }
     }
 
-    static void sort(Comparable[] arr) {
-        total = arr.length - 1;
-        for (int i = total / 2; i >= 0; i--)
-            heapify(arr, i);
-        for (int i = total; i > 0; i--) {
-            swap(arr, 0, i);
-            total--;
-            heapify(arr, 0);
+    public void sort(int arr[]) {
+        int n = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i >= 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
         }
     }
-
-    public static void main(String[] args) {
-        Integer[] arr = {1, 50, 30, 10, 60, 80};
-        print_array('b', arr);
-        sort(arr);
-        print_array('a', arr);
-    }
-
 }
